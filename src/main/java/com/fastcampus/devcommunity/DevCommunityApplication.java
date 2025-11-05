5bc67c3f057cf4c01b9f2ad9af5e9e5a030cff6a
kwihyeon <imleo322@gmail.com>
2025-11-05 15:36:24 +0900

    first commit

---
+++ src/main/java/com/fastcampus/devcommunity/DevCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.devcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevCommunityApplication.class, args);
+    }
+
+}
